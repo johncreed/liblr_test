b1d49128f2bc51028de35b3a656ef9f3599ce2f7
johncreed <hsiajuiyang5174@gmail.com>
2018-03-28 12:14:47 +0800

    Read linear and exp output log

--- script/C_P_relation.py
+++ script/C_P_relation.py
@@ -17,18 +17,28 @@ from os.path import join
 
 home = "/home/johncreed"
 tmp = join(home, "tmp")
-out_path = ""
+out_exp_path = ""
+out_linear_path = ""
 pic_path = ""
 
-def choose_output_folder():
+def choose_exp_folder():
     global pic_path, out_path
-    out_dir = input("Which folder to read out file: ")
+    out_dir = input("Which exp folder to read out file: ")
     if out_dir not in os.listdir(tmp):
       sys.exit('out dir not found!')
     else:
-      out_path = join(tmp, out_dir)
+      out_exp_path = join(tmp, out_dir)
 
-    pic_dir = input("Which folder to store pic: ")
+def choose_linear_folder():
+    global pic_path, out_path
+    out_dir = input("Which linear folder to read out file: ")
+    if out_dir not in os.listdir(tmp):
+      sys.exit('out dir not found!')
+    else:
+      out_linear_path = join(tmp, out_dir)
+
+def choose_pic_folder():
+    pic_dir = "Cmp-Files" + out_exp_path.split('/')[-1] + "--" + out_linear_path.split('/')
     if pic_dir in os.listdir(tmp):
         cmf = input("Replace the elements(y/n) ? ")
         if cmf == 'n':
@@ -38,7 +48,8 @@ def choose_output_folder():
     pic_path = join(tmp, pic_dir)
     return
 
-def draw_3D(file_name):
+
+def result_dict(out_path,file_name):
       cvs = [[],[],[]]  # (p, c, err)
       old = [[],[],[]]
       new = [[],[],[]]
@@ -59,19 +70,29 @@ def draw_3D(file_name):
                 new[0].append(float(tmp[3]))
                 new[1].append(float(tmp[5]))
                 new[2].append(math.log10(float(tmp[-1])))
-
+      
+      return { "cvs" : cvs,
+              "old" : old,
+              "new" : new
+              }
       # To ndarray
       for i in range(3):
         cvs[i] = np.asarray(cvs[i])
         old[i] = np.asarray(old[i])
         new[i] = np.asarray(new[i])
-      
+
+
+
+def draw_3D(file_name):
+      dictL =  result_dict(out_linear_path, file_name)
+      dictE = result_dict(out_exp_path, file_name)
       angle_list = [0,90] # 0 ~ 90
       for angle in angle_list:
           fig=pylab.figure()
           ax = p3.Axes3D(fig)
           pylab.title(tle)
-          ax.scatter3D(cvs[0],cvs[1],cvs[2], s=[1] ,c = "b",marker = '.')
+          ax.scatter3D(dictL["cvs"][0],dictL["cvs"][1],dictL["cvs"][2], s=[1] ,c = "b",marker = '.')
+          ax.scatter3D(dictE["cvs"][0],dictE["cvs"][1],dictE["cvs"][2], s=[1] ,c = "b",marker = 'v')
           ax.scatter3D(old[0],old[1],old[2], s = [80],c = "red", marker = 'o')
           ax.scatter3D(new[0],new[1],new[2], s =  [40],c = "green", marker = 'o')
           ax.set_xlabel('P')
@@ -84,7 +105,8 @@ def draw_3D(file_name):
 
 
 def __main__():
-  choose_output_folder()
+  choose_exp_folder()
+  choose_linear_folder()
   file_names = [f for f in os.listdir(out_path)]
   for file in file_names:
       print ("Do " + file)
