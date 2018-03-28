c1d7687a6e02a80083f494a93fe02db66d2dd06c
johncreed <hsiajuiyang5174@gmail.com>
2018-03-28 15:55:00 +0800

    Modify C_P_relation to draw graph

--- script/C_P_relation.py
+++ script/C_P_relation.py
@@ -22,7 +22,7 @@ out_linear_path = ""
 pic_path = ""
 
 def choose_exp_folder():
-    global pic_path, out_path
+    global out_exp_path
     out_dir = input("Which exp folder to read out file: ")
     if out_dir not in os.listdir(tmp):
       sys.exit('out dir not found!')
@@ -30,7 +30,7 @@ def choose_exp_folder():
       out_exp_path = join(tmp, out_dir)
 
 def choose_linear_folder():
-    global pic_path, out_path
+    global out_linear_path
     out_dir = input("Which linear folder to read out file: ")
     if out_dir not in os.listdir(tmp):
       sys.exit('out dir not found!')
@@ -38,11 +38,14 @@ def choose_linear_folder():
       out_linear_path = join(tmp, out_dir)
 
 def choose_pic_folder():
-    pic_dir = "Cmp-Files" + out_exp_path.split('/')[-1] + "--" + out_linear_path.split('/')
+    global pic_path
+    pic_dir = "Cmp-Files" + out_exp_path.split('/')[-1] + "-" + out_linear_path.split('/')[-1]
     if pic_dir in os.listdir(tmp):
-        cmf = input("Replace the elements(y/n) ? ")
+        cmf = input("rm all the elements(y/n) ? ")
         if cmf == 'n':
             sys.exit("Retry Again")
+        else:
+            os.system("rm "+join(tmp, pic_dir)+"/*")
     else:
         makedirs(join(tmp, pic_dir))
     pic_path = join(tmp, pic_dir)
@@ -53,7 +56,7 @@ def result_dict(out_path,file_name):
       cvs = [[],[],[]]  # (p, c, err)
       old = [[],[],[]]
       new = [[],[],[]]
-      tle = ""
+      best = [[],[],[]]
       min_P = rel_diff  = 0.0
       with open(join(out_path,file_name)) as f:
           for line in f:
@@ -70,46 +73,68 @@ def result_dict(out_path,file_name):
                 new[0].append(float(tmp[3]))
                 new[1].append(float(tmp[5]))
                 new[2].append(math.log10(float(tmp[-1])))
+              if(tmp[0] == "Best"):
+                best[0].append(float(tmp[7]))
+                best[1].append(float(tmp[3]))
+                best[2].append(math.log10(float(tmp[-1])))
       
       return { "cvs" : cvs,
               "old" : old,
-              "new" : new
+              "new" : new,
+              "best" : best
               }
       # To ndarray
       for i in range(3):
         cvs[i] = np.asarray(cvs[i])
         old[i] = np.asarray(old[i])
         new[i] = np.asarray(new[i])
+        best[i] = np.asarray(best[i])
 
 
 
 def draw_3D(file_name):
       dictL =  result_dict(out_linear_path, file_name)
       dictE = result_dict(out_exp_path, file_name)
-      angle_list = [0,90] # 0 ~ 90
+      angle_list = [0,10,20,40,50,60,70,80,90,100,110,120,130,140,140,160,170,180] # 0 ~ 90
+      cnt = "a"
       for angle in angle_list:
           fig=pylab.figure()
           ax = p3.Axes3D(fig)
+          tle = "Compare linear vs exp step"
           pylab.title(tle)
-          ax.scatter3D(dictL["cvs"][0],dictL["cvs"][1],dictL["cvs"][2], s=[1] ,c = "b",marker = '.')
-          ax.scatter3D(dictE["cvs"][0],dictE["cvs"][1],dictE["cvs"][2], s=[1] ,c = "b",marker = 'blue')
-          ax.scatter3D(old[0],old[1],old[2], s = [80],c = "red", marker = 'o')
-          ax.scatter3D(new[0],new[1],new[2], s =  [40],c = "green", marker = 'o')
+          ax.scatter3D(dictE["best"][0],dictE["best"][1],dictE["best"][2], s =  [300],c = "yellow", marker = 'o')
+          ax.scatter3D(dictL["cvs"][0],dictL["cvs"][1],dictL["cvs"][2], s=[30] ,c = "r",marker = 'o')
+          ax.scatter3D(dictE["cvs"][0],dictE["cvs"][1],dictE["cvs"][2], s=[10] ,c = "black",marker = 'o')
+          #ax.scatter3D(dictE["old"][0],dictE["old"][1],dictL["old"][2], s = [80],c = "red", marker = 'o')
+          ax.scatter3D(dictE["new"][0],dictE["new"][1],dictE["new"][2], s =  [80],c = "green", marker = 'o')
           ax.set_xlabel('P')
           ax.set_ylabel('C')
           ax.set_zlabel('Error')
-          ax.view_init(0, angle)
+          ax.view_init(10, angle)
           fig.add_axes(ax)
-          plt.savefig(join(pic_path,file_name+"-"+str(angle)+".eps"), format="eps", dpi=1000)
+          #plt.savefig(join(pic_path,file_name+"-"+str(angle)+".eps"), format="eps", dpi=1000)
+          plt.savefig(join(pic_path,file_name+"-"+cnt+".eps"), format="eps", dpi=1000)
+          cnt += "a"
           plt.close()
 
+
+def do_gif(file_name):
+  cmd = "convert -delay 20 -loop 0 " + "{}.eps".format(join(pic_path, "*")) + " " + "{}.gif".format(join(pic_path,file_name))
+  os.system(cmd)
+  cmd = "rm " + join(pic_path, "*.eps")
+  os.system(cmd)
 
 def __main__():
   choose_exp_folder()
   choose_linear_folder()
+  choose_pic_folder()
+  print (out_exp_path)
+  print (out_linear_path)
+  print (pic_path)
   file_names = [f for f in os.listdir(out_exp_path)]
   for file in file_names:
       print ("Do " + file)
       draw_3D(file)
+      do_gif(file)
 
 __main__()
