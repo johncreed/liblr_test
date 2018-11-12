908511a00b5fcde4f19b55c45e8b1c50df244645
johncreed <hsiajuiyang5174@gmail.com>
2018-11-12 18:16:06 +0800

    Update script

--- script/run.sh
+++ script/run.sh
@@ -1,52 +1,47 @@
 #! /bin/bash
 num_core=96
 #e='1e-2'
-#e='1e-3'
+e='1e-3'
 #e='1e-4'
 #e='1e-5'
-e='1e-6'
+#e='1e-6'
 
 case $1 in 
   110)
     t=11
     s='0'
-    log_path='log/s0_old'.$e
     ext='s0old'
     ;;
   112)
     t=11
     s='2'
-    log_path='log/s2_old'.$e
     ext='s2old'
     ;;
   120)
     t=12
     s='0'
-    log_path='log/s0_new.1e-5'.$e
     ext='s0new'
     ;;
   122)
     t=12
     s='2'
-    log_path='log/s2_new.1e-5'.$e
     ext='s2new'
     ;;
   130)
     t=13
     s='0'
-    log_path='log/s0full'.$e
     ext='s0full'
     ;;
   132)
     t=13
     s='2'
-    log_path='log/s2full'.$e
     ext='s2full'
     ;;
   *)
     echo "Not match"
     exit 1
 esac
+log_path="log/$e.$ext"
 
 mkdir -p $log_path
 grid()
